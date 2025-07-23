ce1b8b872acbc3d4a17eaa4a9f48e2a2b14ce495
Nishka Ranadive <nishka.ranadive@outlook.com>
2025-07-24 00:07:58 +0530

    Update and rename exp-5.cpp to Exp-5 Decision Statements.cpp

---
+++ Exp-5 Decision Statements.cpp
@@ -0,0 +1,31 @@
+//NAME: Nishka Ranadive
+//PRN: 24070123082
+//Exp-5
+
+//Program-1
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    
+    cout<<"Enter a number:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"Number is EVEN.";
+    }
+    else{
+        cout<<"Number is ODD.";
+    }
+
+    return 0;
+}
+
+
+/*==========OUTPUTS==========
+Enter a number:2
+Number is EVEN.
+Enter a number:3
+Number is ODD.
+*/
